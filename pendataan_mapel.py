8076a02cc838f24f777af538e28700d5115824dd
AlfeenX <alfinprj05@gmail.com>
2024-11-13 01:25:01 +0700

    Change variable name

--- pendataan_mapel.py
+++ pendataan_mapel.py
@@ -1,11 +1,11 @@
 print('='*10,'ADMIN','='*10)
-panjangdata= int(input('Masukkan banyak buku yang ingin di input :\n'))
+dataLength= int(input('Masukkan banyak buku yang ingin di input :\n'))
 datas=[]
 
 n=1
-while n <=panjangdata:
-    matapelajaran=input(f'Masukkan buku Ke-{n}:\n')
-    datas.append(matapelajaran)
+while n <=dataLength:
+    inputBook=input(f'Masukkan buku Ke-{n}:\n')
+    datas.append(inputBook)
     n+=1
 
 print('='*37)
@@ -13,8 +13,8 @@ print('berikut adalah daftar buku yang berhasil dimasukkan','\n',datas)
 datas_lower=[data.lower() for data in datas]
 print('='*37)
 
-lanjut=input('Apakah anda ingin lanjut mencari buku? (y/n)\n')
-if lanjut != "y":
+validateContinue=input('Apakah anda ingin lanjut mencari buku? (y/n)\n')
+if validateContinue != "y":
     exit()
 
 print("\n","="*10,'PENCARIAN MAPEL',"="*10)
